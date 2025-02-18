9497fc5e997a444ebd87d8cd8485491516cef745
mv81 <mvidal0001@gmail.com>
2025-02-18 14:08:25 +0100

    AndroidXR ask for x86_64-linux-android on x86_64 PC PR 1170

--- build.rs
+++ build.rs
@@ -254,7 +254,7 @@ fn main() {
     // On Android, we must ensure that we're dynamically linking against the C++ standard library.
     // For more details, see https://github.com/rust-windowing/android-ndk-rs/issues/167
     use std::env::var;
-    if var("TARGET").map(|target| target == "aarch64-linux-android").unwrap_or(false) {
+    if var("TARGET").map(|target| target.ends_with("-linux-android")).unwrap_or(false) {
         cargo_link!("dylib=c++");
     }
 }
